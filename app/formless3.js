ce24966d769757a8a3349f77758a8016d6233be5
Nguyen Hao Hiep Le <hle@c124-02.enst.fr>
2015-10-23 20:29:16 +0200

    update

--- app/formless3.js
+++ app/formless3.js
@@ -5,23 +5,150 @@ var mainApp = angular.module("MainApp",["dndLists","ngRoute","ui.bootstrap"]);
 
 mainApp.controller("FormDesignCtr", function($scope) {
     var DEFAULT_LABEL = "Untitled";
+    var NO_LABEL = "";
+    var REQ_DEFAULT = 'yes';
     $scope.models = {
         selected: null,
         templates: [
-            {type: "item", id: 1, name: "Text"  , icon:'glyphicon-font', label: DEFAULT_LABEL, component: "<input type='text' />", options: []},
-            {type: "item", id: 2, name: "Number", icon:'glyphicon-sound-5-1', label: DEFAULT_LABEL, component: "<input type='number' />", options: []},
-            {type: "item", id: 3, name: "Date"  , icon:'glyphicon-calendar', label: DEFAULT_LABEL, component: "<input type='date' />", options: []},
-            {type: "item", id: 4, name: "Email" , icon:'glyphicon-envelope', label: DEFAULT_LABEL, component: "<input type='email' />", options: []},
-            {type: "item", id: 5, name: "Paragraph", icon:'glyphicon-align-justify', label: DEFAULT_LABEL, component: "<input type='text' />", options: []},
-            {type: "item", id: 6, name: "Radio", icon:'glyphicon-record', label: DEFAULT_LABEL, component: "<input type='number' />", options: []},
-            {type: "item", id: 7, name: "Checkbox(es)"  , icon:'glyphicon-ok-circle', label: DEFAULT_LABEL, component: "<input type='date' />", options: []},
-            {type: "item", id: 8, name: "Dropdown" , icon:'glyphicon-collapse-down', label: DEFAULT_LABEL, component: "<input type='email' />", options: []},
-            {type: "item", id: 9, name: "Signature"  , icon:'glyphicon-pencil', label: DEFAULT_LABEL, component: "<input type='text' />", options: []},
-            {type: "item", id: 10, name: "Attached File(s)", icon:'glyphicon-paperclip', label: DEFAULT_LABEL, component: "<input type='number' />", options: []},
-            {type: "item", id: 11, name: "Header"  , icon:'glyphicon-header', label: DEFAULT_LABEL, component: "<input type='date' />", options: []},
-            {type: "item", id: 12, name: "Section" , icon:'glyphicon-minus', label: DEFAULT_LABEL, component: "<input type='email' />", options: []},
-                    
-            {type: "container", id: 0, name: "Student", columns: [[]]}
+            {
+                type: "item", 
+                id: 0, 
+                name: "Text"  , 
+                icon:'glyphicon-font', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='text' />"            
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Number", 
+                icon:'glyphicon-sound-5-1', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='number' />"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Date"  , 
+                icon:'glyphicon-calendar', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='date' />",
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Email" , 
+                icon:'glyphicon-envelope',
+                required: REQ_DEFAULT, 
+                label: DEFAULT_LABEL, 
+                component: "<input type='email' />"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Paragraph", 
+                icon:'glyphicon-align-justify',
+                required: REQ_DEFAULT, 
+                label: DEFAULT_LABEL, 
+                component: "<textarea rows='5'></textarea>"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Radio", 
+                icon:'glyphicon-record', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='radio' name='{{item.name}}{{item.id}}' value='radio 1'/>Radio 1<input type='radio' name='{{item.name}}{{item.id}}' value='radio 2'/> Radio 2", 
+                options: []
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Checkbox(es)"  , 
+                icon:'glyphicon-ok-circle', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='Checkbox' />Checkbox 1<input type='Checkbox' />Checkbox2", 
+                options: []
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Dropdown" , 
+                icon:'glyphicon-collapse-down', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<select><option>{{item.label}}</option></select>", 
+                field_options: [
+                                    {label: "option 1", checked: "yes"},
+                                    {label: "option 2", checked: "no"}
+                                ]
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Signature"  , 
+                icon:'glyphicon-pencil', 
+                required: REQ_DEFAULT,
+                label: DEFAULT_LABEL, 
+                component: "<input type='file' />"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Attached File(s)", 
+                required: REQ_DEFAULT,
+                icon:'glyphicon-paperclip', 
+                label: DEFAULT_LABEL, 
+                component: "<input type='file' multiple/>"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Header"  , 
+                icon:'glyphicon-header', 
+                label: NO_LABEL, 
+                component: "<h1>Header of the form</h1>"
+            },
+            {
+                type: "item", 
+                id: 0, 
+                name: "Section", 
+                icon:'glyphicon-minus', 
+                label: NO_LABEL, 
+                component: "<h3>Section of a part</h3>"
+            },       
+            {
+                type: "container", 
+                id: 1, 
+                name: "Student", 
+                properties: [
+                    {name: "Nom", type: "text"},
+                    {name: "Prenom", type: "text"},
+                    {name: "Date de naissance", type: "date"},
+                    {name: "Sexe", type: "text"}
+                    ]
+                ,
+                columns: [[]]
+            }
+            ,            
+            {
+                type: "container", 
+                id: 1, 
+                name: "Teacher", 
+                columns: [[]]
+            }
+            ,            
+            {
+                type: "container", 
+                id: 1, 
+                name: "Project", 
+                columns: [[]]
+            }
         ],
         dropzones: {
             "Form Design": [
@@ -31,6 +158,7 @@ mainApp.controller("FormDesignCtr", function($scope) {
 
     $scope.$watch('models.dropzones', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
+        console.log($scope.modelAsJson);
     }, true);
 
 });
@@ -46,10 +174,33 @@ mainApp.directive('htmlRender', function($compile) {
         wrapper.html(value);
         var markup = $compile(wrapper.contents())(scope);
         element.append(markup)
+        console.log(markup);
       });
     }
   };
 })
+
+mainApp.directive('bindUnsafeHtml', ['$compile', function ($compile) {
+      return function(scope, element, attrs) {
+          scope.$watch(
+            function(scope) {
+              // watch the 'bindUnsafeHtml' expression for changes
+              return scope.$eval(attrs.bindUnsafeHtml);
+            },
+            function(value) {
+              // when the 'bindUnsafeHtml' expression changes
+              // assign it into the current DOM
+              element.html(value);
+
+              // compile the new DOM and link it to the current
+              // scope.
+              // NOTE: we only compile .childNodes so that
+              // we don't get into infinite loop compiling ourselves
+              $compile(element.contents())(scope);
+            }
+        );
+    };
+}]);
 
 mainApp.controller("FileMenuCtr", function($scope){
     $scope.commands = [ {name: "Open", icon: "glyphicon-folder-open"},
