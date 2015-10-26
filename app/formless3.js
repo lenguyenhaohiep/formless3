44a1242e00f472e9389d8b09785e61e89dcf7559
Nguyen Hao Hiep Le <hle@c129-05.enst.fr>
2015-10-26 20:57:32 +0100

    update

--- app/formless3.js
+++ app/formless3.js
@@ -4,16 +4,21 @@
 var mainApp = angular.module("MainApp",["dndLists","ngRoute","ui.bootstrap"]);
 
 mainApp.controller("FormDesignCtr", function($scope) {
+    /**
+    *   Some default values
+    */
     var DEFAULT_LABEL = "Untitled";
     var NO_LABEL = "";
     var REQ_DEFAULT = 'yes';
-    var PROPERTIES_DEFAULT = "<div class='option-item' ng-repeat='option in item.field_options'>\
-                                <input type='checkbox' ng-checked='{{option.checked}}' ng-model='option.checked'/>\
+    var PROPERTIES_DEFAULT ="<div class='option-item' ng-repeat='option in item.field_options'>\
+                                <input type='checkbox' ng-checked='{{option.checked}}' ng-model='option.checked' ng-click='$parent.$parent.setSelect(item, option)'/>\
                                 <input type='text' value='{{option.label}}' ng-model='option.label'/>\
-                                <a class='mini-item item-add glyphicon-plus'></a>\
-                                <a class='mini-item item-remove glyphicon-minus'></a>\
-                            </div>\
-                            <button type='button' class='btn btn-primary btn-xs'>Add option</button>";
+                                <a class='mini-item item-add glyphicon-plus' ng-click='$parent.$parent.addOption(item, $index+1)'></a>\
+                                <a class='mini-item item-remove glyphicon-minus' ng-click='$parent.$parent.removeOption(item, $index)'></a>\
+                                </div>\
+                                <button type='button' class='btn btn-primary btn-xs' ng-click='$parent.$parent.addOption(item, -1)'>Add option</button>";
+    var DEFAULT_LABEL_OPTION = "Option";
+    var DEFAULT_CHECK_OPTION = false;
 
 
     $scope.models = {
@@ -26,7 +31,8 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-font', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<input type='text' />"            
+                value: null,
+                component: "<input type='text' class='form-control' ng-model='item.value' />"            
             },
             {
                 type: "item", 
@@ -35,7 +41,8 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-sound-5-1', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<input type='number' />"
+                value: null,
+                component: "<input type='number' class='form-control' ng-model='item.value'/>"
             },
             {
                 type: "item", 
@@ -44,7 +51,8 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-calendar', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<input type='date' />",
+                value: null,
+                component: "<input type='date' class='form-control' ng-model='item.value'/>",
             },
             {
                 type: "item", 
@@ -53,7 +61,8 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-envelope',
                 required: REQ_DEFAULT, 
                 label: DEFAULT_LABEL, 
-                component: "<input type='email' />"
+                value: null,
+                component: "<input type='email' pattern='([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})' class='form-control' ng-model='item.value'/>"
             },
             {
                 type: "item", 
@@ -62,7 +71,8 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-align-justify',
                 required: REQ_DEFAULT, 
                 label: DEFAULT_LABEL, 
-                component: "<textarea rows='5'></textarea>"
+                value: null,
+                component: "<textarea rows='5' class='form-control' ng-model='item.value' /></textarea>"
             },
             {
                 type: "item", 
@@ -71,12 +81,12 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-record', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<input type='radio' name='{{item.name}}{{item.id}}' ng-model='option.checked' ng-repeat-start='option in item.field_options' value='{{option.checked}}' ng-checked='{{option.checked}}'/>\
-                                <span ng-model='option.label'>{{option.label}}</span><br ng-repeat-end/>", 
+                component: "<input type='radio' name='{{item.name}}{{item.id}}' ng-repeat-start='option in item.field_options' ng-model='item.value' ng-value='option' ng-click='$parent.$parent.setSelect(item, option)'/><span ng-model='option.label'>{{option.label}}</span><br ng-repeat-end/>", 
                 field_options:[
-                            {label: "Radio 1", checked: true},
-                            {label: "Radio 2", checked: true}
+                            {label: "Radio 1", checked: false},
+                            {label: "Radio 2", checked: false}
                         ],
+                value: null,
                 properties: PROPERTIES_DEFAULT
             },
             {
@@ -86,12 +96,12 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-ok-circle', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<input type='checkbox' ng-model='option.checked' ng-repeat-start='option in item.field_options' value='{{option.label}}' ng-checked='{{option.checked}}'/>\
-                                <span ng-model='option.label'>{{option.label}}</span><br ng-repeat-end />", 
+                component: "<input type='checkbox' ng-model='option.checked' ng-repeat-start='option in item.field_options' value='{{option.label}}' ng-checked='{{option.checked}}' ng-click='$parent.$parent.setSelect(item, option)'/><span ng-model='option.label'>{{option.label}}</span><br ng-repeat-end />", 
                 field_options:[
-                            {label: "Checkbox 1", checked: true},
+                            {label: "Checkbox 1", checked: false},
                             {label: "Checkbox 2", checked: false}
                         ],
+                value: null,
                 properties: PROPERTIES_DEFAULT
             },
             {
@@ -101,13 +111,12 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-collapse-down', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
-                component: "<select>\
-                                <option ng-repeat='option in item.field_options' value='{{option.label}}'>{{option.label}}</option>\
-                             </select>", 
+                component: "<select class='form-control' ng-model='item.value' ng-options='option.label for option in item.field_options'></select>", 
                 field_options: [
-                                    {label: "Option 1", checked: true},
+                                    {label: "Option 1", checked: false},
                                     {label: "Option 2", checked: false}
                                 ],
+                value: null,
                 properties: PROPERTIES_DEFAULT
             },
             {
@@ -117,6 +126,7 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 icon:'glyphicon-pencil', 
                 required: REQ_DEFAULT,
                 label: DEFAULT_LABEL, 
+                value: null,
                 component: "<input type='file' />"
             },
             {
@@ -125,24 +135,27 @@ mainApp.controller("FormDesignCtr", function($scope) {
                 name: "Attached File(s)", 
                 required: REQ_DEFAULT,
                 icon:'glyphicon-paperclip', 
-                label: DEFAULT_LABEL, 
+                label: DEFAULT_LABEL,
+                value: null, 
                 component: "<input type='file' multiple/>"
             },
             {
                 type: "item", 
+                noinput: true,
                 id: 0, 
                 name: "Header"  , 
                 icon:'glyphicon-header', 
-                label: NO_LABEL, 
-                component: "<h1>Header of the form</h1>"
+                label: "Header of the form", 
+                component: "<h1>{{item.label}}</h1>"
             },
             {
                 type: "item", 
+                noinput: true,
                 id: 0, 
                 name: "Section", 
                 icon:'glyphicon-minus', 
-                label: NO_LABEL, 
-                component: "<h3>Section of a part</h3>"
+                label: "Section", 
+                component: "<h3>{{item.label}}</h3>"
             },       
             {
                 type: "container", 
@@ -173,28 +186,92 @@ mainApp.controller("FormDesignCtr", function($scope) {
             }
         ],
         dropzones: {
-            "Form Design": [
+            DEFAULT_ZONE: [
             ]
         }
     };
 
+
+    /**
+    *   When an options for checkbox/radio/dropdown is selected, we mark them as checked=true
+    */
+    $scope.setSelect = function(item, option){
+        if (item.name != 'Checkbox'){
+            alert(item.name);
+            angular.forEach(item.field_options, function(i){
+                i.checked = false;
+            });
+            option.checked = true;
+
+            item.value = option;
+        }
+        else{
+            item.value = [];
+            angular.forEach(item.field_options, function(i){
+                if (i.checked)
+                    item.value.push(i);
+            });
+        }
+    };
+
+
+    /**
+    *   Add more options for checkbox/radio/dropdown
+    *   item is an object which represents a field/container
+    *   When index=-1, we add an option at the end
+    */
+    $scope.addOption = function(item, index){
+        var new_option = {label: DEFAULT_LABEL, checked: DEFAULT_CHECK_OPTION};
+        //Last of the list
+        if (index == -1){
+            index = item.field_options.length;
+        }
+        //At after index
+        item.field_options.splice(index,0,new_option);
+
+    };
+
+
+    /**
+    *   Remove an option at index for the current item
+    */
+    $scope.removeOption = function(item, index){
+        item.field_options.splice(index, 1);
+    };
+
+    /*
+    *   Delete an item in the current model
+    */
+    $scope.removeItem = function(index) {
+        $scope.models.dropzones.DEFAULT_ZONE.splice(index,1);
+        $scope.models.selected = null;
+    }
+
     $scope.$watch('models.dropzones', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
-        console.log($scope.modelAsJson);
+        //console.log($scope.modelAsJson);
     }, true);
 
 });
 
+
+/**
+* This directive is used to render code HTML from text to the page, this method is unsafe method, pay attention in use
+* This plugin aims to work only in the offline mode, so this method has no risk in this case
+*/
 mainApp.directive('htmlRender', function($compile, $sce) {
   return {
     restrict: 'E',
     transclude: true,
     scope: { item: '=' , val: '@'},
     link: function(scope, element) {
-
         var value = scope.item[scope.val];
-        if (!value) return;        
+        if (!value) return;
+
+        //Wrap in case text only so that the compile can work without error        
         var wrapper = angular.element('<div>');value
+
+        //Trusted HTML
         value=$sce.trustAsHtml(value);
         wrapper.html(value);
         var markup = $compile(wrapper.contents())(scope);
@@ -204,27 +281,25 @@ mainApp.directive('htmlRender', function($compile, $sce) {
   }
 });
 
-mainApp.directive('bindUnsafeHtml', ['$compile', function ($compile) {
-      return function(scope, element, attrs) {
-          scope.$watch(
-            function(scope) {
-              // watch the 'bindUnsafeHtml' expression for changes
-              return scope.$eval(attrs.bindUnsafeHtml);
-            },
-            function(value) {
-              // when the 'bindUnsafeHtml' expression changes
-              // assign it into the current DOM
-              element.html(value);
+/**
+* When we enter the mouse in every field/ container in the form, the "remove" button will appear and allow us delete current field/container
+*/
+mainApp.directive('flItem', function (){
+    return function(scope, element, attr){
+        var delete_button = angular.element(element[0].querySelector('.item-fix'));
 
-              // compile the new DOM and link it to the current
-              // scope.
-              // NOTE: we only compile .childNodes so that
-              // we don't get into infinite loop compiling ourselves
-              $compile(element.contents())(scope);
-            }
-        );
-    };
-}]);
+        //Upon mouse leaves
+        element.on('mouseleave',function(){
+            delete_button.css("display","none");
+        });
+
+        //Upon mouse enters
+        element.on('mouseenter', function(){
+            delete_button.css("display","block");
+        });
+    }
+});
+
 
 mainApp.controller("FileMenuCtr", function($scope){
     $scope.commands = [ {name: "Open", icon: "glyphicon-folder-open"},
