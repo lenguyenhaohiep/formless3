38c7091bf4a1d13b733f6f7f9ce43d5801218bc2
Hiep LE <hieple@MacBook-de-Hiep.local>
2016-02-25 23:41:48 +0100

    delete files

--- background.js
+++
@@ -1,8 +0,0 @@
-try{
-    chrome.runtime.onMessage.addListener(
-    	function(request, sender, sendResponse) {
-    		localStorage.data = request.data;
-    		localStorage.job = request.job;
-    });
-} catch (err){
-}
