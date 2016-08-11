6675461263f6b0de57eb78e912f88c25e4297e36
Hiep Le <hle@Hieps-MacBook.local>
2016-08-11 02:00:32 +0200

    fix signing

--- app/portableform.js
+++ app/portableform.js
@@ -9,6 +9,8 @@ function loadjs(filename){
     var fileref=document.createElement('script')
     fileref.setAttribute("type","text/javascript")
     fileref.setAttribute("src", filename)
+    if (typeof fileref!="undefined")	
+        document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
 
